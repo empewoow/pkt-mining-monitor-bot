7ad51500ef6c19c391fa69213f58b5bba03484f1
Matthijs <empewoow@gmail.com>
2022-12-22 13:11:00 +0100

    Better formatting for large numbers; added 1,000 comma separators.

--- miner_info.py
+++ miner_info.py
@@ -21,14 +21,14 @@ def get_miner_info(pkt_addresses):
             mined24 = int(data["mined24"]) / (2**30)
             unconsolidated_txns = int(data["balanceCount"])
 
-            result += "Address balance: " + "%.2f" % balance + " PKT\n"
+            result += "Address balance: " + f"{balance:,.2f}" + " PKT\n"
             result += "Unconsolidated Txns: " + str(unconsolidated_txns) + "\n"
 
             if (unconsolidated_txns > 1200):
                 result += "You should fold your coins again!\n"
 
             result += "\n"
-            result += "Mined last 24 hours: " + "%.2f" % mined24 + " PKT\n"
+            result += "Mined last 24 hours: " + f"{mined24:,.2f}" + " PKT\n"
 
         with urllib.request.urlopen("https://explorer.pkt.cash/api/v1/PKT/pkt/address/" + pkt_address + "/income/30") as url:
             data = json.load(url)
@@ -37,8 +37,8 @@ def get_miner_info(pkt_addresses):
             received_7daysago = int(data["results"][7]["received"]) / (2**30)
             received_30daysago = int(data["results"][30]["received"]) / (2**30)
 
-            result += "Mined yesterday: " + "%.2f" % received_yesterday + " PKT\n"
-            result += "Mined 7 days ago: " + "%.2f" % received_7daysago + " PKT\n"
-            result += "Mined 30 days ago: " + "%.2f" % received_30daysago + " PKT\n"
+            result += "Mined yesterday: " + f"{received_yesterday:,.2f}" + " PKT\n"
+            result += "Mined 7 days ago: " + f"{received_7daysago:,.2f}" + " PKT\n"
+            result += "Mined 30 days ago: " + f"{received_30daysago:,.2f}" + " PKT\n"
 
     return result
