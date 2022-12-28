f9ef12b9b0740d959ba5ddf8da82fd0426d78f66
Matthijs <empewoow@gmail.com>
2022-12-28 14:18:25 +0100

    Handle invalid PKT addresses.

--- mining_data.py
+++ mining_data.py
@@ -18,32 +18,43 @@ def get_mining_data(addresses):
             number += 1
 
             result += "\nAddress #" + str(number) + ": " + address + "\n"
+            failed = 0
 
-            with urllib.request.urlopen("https://explorer.pkt.cash/api/v1/PKT/pkt/address/" + address) as url:
-                data = json.load(url)
+            try:
+                with urllib.request.urlopen("https://explorer.pkt.cash/api/v1/PKT/pkt/address/" + address) as url:
+                    data = json.load(url)
 
-                balance = int(data["balance"]) / (2**30)
-                mined24 = int(data["mined24"]) / (2**30)
-                unconsolidated_txns = int(data["balanceCount"])
+                    balance = int(data["balance"]) / (2**30)
+                    mined24 = int(data["mined24"]) / (2**30)
+                    unconsolidated_txns = int(data["balanceCount"])
 
-                result += "Address balance: " + f"{balance:,.2f}" + " PKT\n"
-                result += "Unconsolidated Txns: " + str(unconsolidated_txns)
+                    result += "Address balance: " + f"{balance:,.2f}" + " PKT\n"
+                    result += "Unconsolidated Txns: " + str(unconsolidated_txns)
 
-                if (unconsolidated_txns > 1200):
-                    result += " ⚠️ <i>Fold coins!</i>"
+                    if (unconsolidated_txns > 1200):
+                        result += " ⚠️ <i>Fold coins!</i>"
 
-                result += "\n"
-                result += "Mined last 24 hours: " + f"{mined24:,.2f}" + " PKT\n"
+                    result += "\n"
+                    result += "Mined last 24 hours: " + f"{mined24:,.2f}" + " PKT\n"
+            
+            except EnvironmentError:
+                failed = 1
+                result += "Not a valid PKT address..."
 
-            with urllib.request.urlopen("https://explorer.pkt.cash/api/v1/PKT/pkt/address/" + address + "/income/30") as url:
-                data = json.load(url)
+            if failed == 0:
+                try:
+                    with urllib.request.urlopen("https://explorer.pkt.cash/api/v1/PKT/pkt/address/" + address + "/income/30") as url:
+                        data = json.load(url)
 
-                received_yesterday = int(data["results"][0]["received"]) / (2**30)
-                received_7daysago = int(data["results"][7]["received"]) / (2**30)
-                received_30daysago = int(data["results"][30]["received"]) / (2**30)
+                        received_yesterday = int(data["results"][0]["received"]) / (2**30)
+                        received_7daysago = int(data["results"][7]["received"]) / (2**30)
+                        received_30daysago = int(data["results"][30]["received"]) / (2**30)
 
-                result += "Mined yesterday: " + f"{received_yesterday:,.2f}" + " PKT\n"
-                result += "Mined 7 days ago: " + f"{received_7daysago:,.2f}" + " PKT\n"
-                result += "Mined 30 days ago: " + f"{received_30daysago:,.2f}" + " PKT\n"
+                        result += "Mined yesterday: " + f"{received_yesterday:,.2f}" + " PKT\n"
+                        result += "Mined 7 days ago: " + f"{received_7daysago:,.2f}" + " PKT\n"
+                        result += "Mined 30 days ago: " + f"{received_30daysago:,.2f}" + " PKT\n"
+                
+                except EnvironmentError:
+                    result += ""
 
         return result
